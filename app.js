5726643c497262321c238d5bf8e0f2e137944d84
nabilhatami <132873660+nabilhatami86@users.noreply.github.com>
2024-11-08 13:23:31 +0700

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,23 @@ const router = require ('./routes')
 app.use(express.json())
 app.use(router)
 
+app.get('/', (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Halaman HTML</title>
+    </head>
+    <body>
+        <h1>Selamat Datang di Halaman HTML</h1>
+        <p>Ini adalah halaman HTML yang dikirim dari Express.</p>
+    </body>
+    </html>
+  `);
+});
+
 app.listen(3000, () =>
   console.log(`Server berjalan di http://localhost:3000 }`)
 );
